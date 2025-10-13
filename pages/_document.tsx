0675b48ac09e1f133c3821f4337f6b8e5d0e2a48
Siriuslicht <laurairlean@gmail.com>
2025-10-13 23:23:34 +0500

    feat: meta tags integration on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+        {/* SEO */}
+
+        <meta name="keyword" content={"nestar , nestar.uz, devex mern, mern nestjs fullstack"} />
+        <meta name="description" content={
+          "Buy and sell properties anywhere anytime in Tashkent. | " +
+          "Покупайте и продавайте недвижимость в любом месте и в любое время в Ташкенте. | " +
+          "Toshkentning istalgan joyidan ko'chmas mulk sotib oling va soting"
+        } />
+      </Head>
       <body>
         <Main />
         <NextScript />
